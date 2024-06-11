1e19425afaf5d5269951cba49dabdb5b56ef6253
Davi Munhoz <davimunhoz1005@gmail.com>
2024-06-11 19:26:26 -0300

    Creating Application With Spring Initializer

---
+++ src/main/java/br/com/dowloadAndUploadFiles/FilesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dowloadAndUploadFiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesApiApplication.class, args);
+	}
+
+}
