55349fd8fea341039bff2b1b59237d74f8407f3c
Davi Munhoz <davimunhoz1005@gmail.com>
2024-06-11 20:07:00 -0300

    Creation of file entity attributes

--- src/main/java/br/com/dowloadAndUploadFiles/entities/File.java
+++ src/main/java/br/com/dowloadAndUploadFiles/entities/File.java
@@ -1,4 +1,30 @@
 package br.com.dowloadAndUploadFiles.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TB_FILE")
+@Builder
 public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "The field name cannot be empty")
+    private String name;
+
+    @NotNull(message = "The field version cannot be empty")
+    private Integer version;
+
+    @NotNull(message = "The field validity cannot be empty")
+    private LocalDate validity;
 }
