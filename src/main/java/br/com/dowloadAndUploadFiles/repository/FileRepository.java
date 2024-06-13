d74073e8bcce61fd5d3965a5340d65e5be4432a6
Davi Munhoz <davimunhoz1005@gmail.com>
2024-06-13 09:49:17 -0300

    Merge service creation and repository modification

--- src/main/java/br/com/dowloadAndUploadFiles/repository/FileRepository.java
+++ src/main/java/br/com/dowloadAndUploadFiles/repository/FileRepository.java
@@ -4,6 +4,11 @@ import br.com.dowloadAndUploadFiles.entities.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    List<File> listFilesByName(String name);
+    void deleteFileByName(String name);
 }
