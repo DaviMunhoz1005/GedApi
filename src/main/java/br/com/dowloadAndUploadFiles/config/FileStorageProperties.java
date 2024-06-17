03dc4c3e124ff0373e7552fa2d902e0484d94707
Davi Munhoz <davimunhoz1005@gmail.com>
2024-06-17 06:21:25 -0300

    Merging develop adding property to store the files and the FileStorageProperties configuration class

---
+++ src/main/java/br/com/dowloadAndUploadFiles/config/FileStorageProperties.java
@@ -0,0 +1,15 @@
+package br.com.dowloadAndUploadFiles.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+@Getter
+@Setter
+public class FileStorageProperties {
+
+    private String uploadDirectory;
+}
