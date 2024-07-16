4f263a9d06e2d86e2bd18a805155041e7e198055
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-07-16 19:47:42 -0300

    Creating the repository of Client and Employee entities

--- src/main/java/br/com/api/repository/ClientRepository.java
+++ src/main/java/br/com/api/repository/ClientRepository.java
@@ -1,15 +1,11 @@
 package br.com.api.repository;
 
-import br.com.api.entities.File_;
-
+import br.com.api.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface FileRepository extends JpaRepository<File_, UUID> {
-
-    List<File_> findByName(String filename);
+public interface ClientRepository extends JpaRepository<Client, UUID> {
 }
--- src/main/java/br/com/api/repository/EmployeeRepository.java
+++ src/main/java/br/com/api/repository/EmployeeRepository.java
@@ -1,15 +1,12 @@
 package br.com.api.repository;
 
-import br.com.api.entities.File_;
-
+import br.com.api.entities.Client;
+import br.com.api.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface FileRepository extends JpaRepository<File_, UUID> {
-
-    List<File_> findByName(String filename);
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
